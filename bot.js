2d47dca03467a26eab333f43ba5c1c74aded9234
salah36 <52756545+salah36@users.noreply.github.com>
2019-07-10 20:38:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Gta Mods By Wail`,"http://twitch.tv/Death Shop")
+client.user.setGame(`%help By ASpro`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
